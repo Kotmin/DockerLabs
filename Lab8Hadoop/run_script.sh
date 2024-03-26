2322db9be1b32b01b53f995ad99d0a4cc3eea3be
kotmin <kotmin@users.noreply.github.com>
2024-03-26 19:37:06 +0100

    add compile sh

--- Lab8Hadoop/run_script.sh
+++ Lab8Hadoop/run_script.sh
@@ -1,3 +1,7 @@
+# Kompilujemy obecny stan WordCountApp
+./compile_jar.sh
+
+
 # Uruchamiamy nasz "manualnie stworzony Hadoop Container"
 docker build -t hadoop-custom:latest .
 docker run -d --name hadoop-custom-container -p 9870:9870 -p 8088:8088 hadoop-custom:latest
