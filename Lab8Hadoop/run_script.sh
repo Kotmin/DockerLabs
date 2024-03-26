bd014fee273b3a73eb5a8e3985a51a6c241c4f5e
kotmin <kotmin@users.noreply.github.com>
2024-03-26 19:35:37 +0100

    to validate run_script

--- Lab8Hadoop/run_script.sh
+++ Lab8Hadoop/run_script.sh
@@ -5,5 +5,28 @@ docker run -d --name hadoop-custom-container -p 9870:9870 -p 8088:8088 hadoop-cu
 # Uruchomienie Dockerfile z DockerHub'a
 docker run -d --name hadoop-dockerhub-container -p 50070:50070 -p 8088:8088 sequenceiq/hadoop-docker:2.7.1
 
+# Skopiowanie pliku .jar do kontenera z DockerHub'a
+
+docker cp wordcount-1.0-SNAPSHOT-jar-with-dependencies.jar hadoop-dockerhub-container:/usr/local/hadoop/
+
+
+
+# Sprawdzenie bazowego użycia zasobów
+docker stats hadoop-custom-container hadoop-dockerhub-container
+
+
+# Uruchomienie
+
+docker exec -it hadoop-custom-container bash
+# Wewnątrz kontenera
+cd $HADOOP_HOME
+hadoop fs -copyFromLocal ./input.txt /input.txt
+hadoop jar wordcount-1.0-SNAPSHOT-jar-with-dependencies.jar WordCount /input.txt /output
+
+docker exec -it hadoop-dockerhub-container bash
+# Wewnątrz kontenera
+cd $HADOOP_HOME
+hadoop fs -copyFromLocal ./input.txt /input.txt
+hadoop jar wordcount-1.0-SNAPSHOT-jar-with-dependencies.jar WordCount /input.txt /output
 
 
